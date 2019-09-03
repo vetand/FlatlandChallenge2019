bf4cbb51b4052371d97d64472459b1f1d1ae5707
vetrov_andrew <vetand2013@yandex.ru>
2019-09-03 19:50:04 +0000

    Update run.py

--- run.py
+++ run.py
@@ -3,8 +3,9 @@ from flatland.envs.observations import TreeObsForRailEnv
 from flatland.envs.predictions import ShortestPathPredictorForRailEnv
 import heapq
 import numpy as np
-
-
+from flatland.envs.generators import complex_rail_generator
+from flatland.envs.rail_env import RailEnv
+from flatland.utils.rendertools import RenderTool
 
 #####################################################################
 # Instantiate a Remote Client
@@ -18,127 +19,340 @@ remote_client = FlatlandRemoteClient()
 # compute the necessary action for this step for all (or even some)
 # of the agents
 #####################################################################
-def heuristic(x1, y1, x2, y2):
-    return abs(x2 - x1) + abs(y2 - y1)
-
-class Node:
-    
-    def __init__(self, env, input_x, input_y, orientation, finish_x, finish_y, input_g):
-        self.id = env.width * env.height * orientation + input_y * env.height + input_x
-        self.x = input_x
-        self.y = input_y
-        self.dest = orientation
-        self.g_value = input_g
-        self.h_value = heuristic(input_x, input_y, finish_x, finish_y)
-        self.f_value = self.h_value + self.g_value
-        
-    def __lt__(self, other):
-        if self.f_value == other.f_value:
-            return self.g_value < other.g_value
-        else:
-            return self.f_value < other.f_value
-
 answer_build = False
 agent_action = []
 current_pos = []
 
-def build(env):
-    global agent_action
-    last_visit = []
-    for x in range(env.height):
-        last_visit.append([-1] * env.width)
-    
-    agent_action = []
-    for ind in range(env.get_num_agents()):
-        agent_action.append([])
-    
-    for ind in range(env.get_num_agents()):
-        previous = []
-        distance = []
-        for dest in range(4):
-            previous.append([])
-            distance.append([])
-        for dest in range(4):
-            for x in range(env.height):
-                previous[dest].append([])
-                distance[dest].append([-1] * env.width)
-        for dest in range(4):
-            for x in range(env.height):
-                for y in range(env.width):
-                    previous[dest][x].append([-1, -1, -1])
+class Map:
+    def __init__(self):
+        self.height = -1
+        self.width = -1
+        self.cellSize = -1
+        self.grid = []
 
-        agent = env.agents[ind]
-        OPEN = []
-        CLOSED = set()
-        USED = set()
-        start = Node(env, agent.position[0], agent.position[1], agent.direction, agent.target[0], agent.target[1], 0)
-        USED.add(start.id)
-        finish_x = agent.target[0]
-        finish_y = agent.target[1]
-        heapq.heappush(OPEN, start)
-        orientation = 0
-        while (len(OPEN) != 0):
-            current = heapq.heappop(OPEN)
-            parent = previous[current.dest][current.x][current.y]
-            CLOSED.add(current.id)
-            if (current.x == finish_x and current.y == finish_y):
-                orientation = current.dest
-                break
-            position = [current.x, current.y]
-            available = env.rail.get_transitions(*position, current.dest)
-            candidates = []
-            if (available[0] == True):
-                candidates.append(Node(env, current.x - 1, current.y, 0, finish_x, finish_y, current.g_value + 1))
-            if (available[1] == True):
-                candidates.append(Node(env, current.x, current.y + 1, 1, finish_x, finish_y, current.g_value + 1))
-            if (available[2] == True):
-                candidates.append(Node(env, current.x + 1, current.y, 2, finish_x, finish_y, current.g_value + 1))
-            if (available[3] == True):
-                candidates.append(Node(env, current.x, current.y - 1, 3, finish_x, finish_y, current.g_value + 1))
-            for neighbor in candidates:
-                if (neighbor.id not in USED or distance[neighbor.dest][neighbor.x][neighbor.y] > current.g_value + 1):
-                    USED.add(neighbor.id)
-                    previous[neighbor.dest][neighbor.x][neighbor.y] = [current.dest, current.x, current.y]
-                    heapq.heappush(OPEN, neighbor)
-                    distance[neighbor.dest][neighbor.x][neighbor.y] = current.g_value + 1
-        start_x = agent.position[0]
-        start_y = agent.position[1]
-        path = []
-        while (finish_x != start_x or finish_y != start_y):
-            path.append([orientation, finish_x, finish_y])
-            parent_node = previous[orientation][finish_x][finish_y]
-            finish_x = parent_node[1]
-            finish_y = parent_node[2]
-            orientation = parent_node[0]
-        path.append([orientation, finish_x, finish_y])
-        path.reverse()
+    def getMapOptions(self):
+        global env
 
-        time = -1
-        for node_num in range(1, len(path)):
-            node = path[node_num]
-            x = node[1]
-            y = node[2]
-            dest = node[0]
-            next_time = max(time, last_visit[x][y]) + 1
-            last_visit[path[node_num - 1][1]][path[node_num - 1][2]] = next_time - 1
-            while time < next_time - 1:
-                time += 1
-                agent_action[ind].append(4)
-            time += 1
-            pre_dest = path[node_num - 1][0]
-            if (abs(pre_dest - dest) % 2 == 0):
-                agent_action[ind].append(2)
-            elif ((pre_dest + 1) % 4 == dest):
-                agent_action[ind].append(3)
+        self.height = env.height
+        
+        self.width = env.width
+
+        self.cellSize = 1
+
+
+class Options:
+    def __init__(self):
+        self.metricType = "manhattan"
+        self.searchType = "astar"
+        self.hweight = 1
+        self.breakingTies = "g-max"
+        self.allowSqueeze = False
+        self.allowDiagonal = False
+        self.cutCorners = False
+
+    def getOptions(self):
+        self.metricType = "manhattan"
+        self.searchType = "astar"
+        self.hweight = 1
+        self.breakingTies = "g-max"
+        self.allowSqueeze = False
+        self.allowDiagonal = False
+        self.cutCorners = False
+
+
+class Agent:
+    def __init__(self, agentId):
+        self.start_i = -1
+        self.start_j = -1
+        self.fin_i = -1
+        self.fin_j = -1
+        self.agentId = agentId
+
+    def getAgent(self):
+        global env
+        self.start_i = env.agents[self.agentId].position[0]
+        self.start_j = env.agents[self.agentId].position[1]
+
+
+        self.fin_i = env.agents[self.agentId].target[0]
+        self.fin_j = env.agents[self.agentId].target[1]
+
+        self.dir = env.agents[self.agentId].direction
+
+
+class Agents:
+    def __init__(self):
+        self.allAgents = []
+        self.size = 0
+
+    def getAgents(self):
+        global env
+        self.size = env.get_num_agents()
+
+
+class Node:
+    def __init__(self, i, j, dir):
+        self.i = i
+        self.j = j
+        self.t = 0
+        self.f = 0
+        self.g = 0
+        self.h = 0
+        self.dir = dir
+        self.parent = None
+
+    def __eq__(self, other):
+        return self.i == other.i and self.j == other.j and self.t == other.t and self.dir == other.dir
+
+    def __ne__(self, other):
+        return not (self.i == other.i and self.j == other.j and self.t == other.t and self.dir == other.dir)
+
+    def __lt__(self, other):
+        if (self.i == other.i):
+                if (self.j == other.j):
+                        return self.dir < other.dir
+                return self.j < other.j
+        return self.i < other.i
+
+    def __hash__(self):
+        return hash((self.i, self.j, self.t, self.dir))
+
+
+class Entry:
+    def __init__(self, priority, value, options):
+        self.priority = priority
+        self.value = value
+        self.breakingTies = options.breakingTies
+
+    def cmp(self, one, two):
+        if (one.f == two.f):
+            if (one.g == two.g):
+                return one < two
             else:
-                agent_action[ind].append(1)
+                if (self.breakingTies == "g-min"):
+                    return one.g < two.g
+                return one.g > two.g
+        return one.f < two.f
 
+    def __cmp__(self, other):
+        return self.cmp(self.priority, other.priority)
+
+    def __lt__(self, other):
+        return self.cmp(self.priority, other.priority)
+
+    def __eq__(self, other):
+        return self.priority == other.priority
+
+
+class ISearch:
+    def __init__(self, options):
+        global env
+        self.lppath = []
+        self.hppath = []
+        self.printGrid = []
+        self.reservations = [set()]
+        self.breakingTies = options.breakingTies
+        self.sqrtTwo = 1.41421356237
+        self.maxTime = 400
+
+    def startAllAgents(self, map, options, agents):
+        self.initReservations(map)
+
+        for i in range(agents.size):
+            self.startSearch(map, options, agents.allAgents[i])
+
+    def initPrintGrid(self, map):
+        self.printGrid.clear()
+        for i in range(map.height):
+            line = [0] * map.width
+            for j in range(map.width):
+                val = map.getValue(i, j)
+                if val == 0:
+                    line[j] = -2
+                else:
+                    line[j] = -1
+            self.printGrid.append(line)
+
+    def initReservations(self, map):
+        self.reservations = sets = [set() for i in range(self.maxTime)]
+
+    def checkReservation(self, i, j, t):
+        return ((i, j) in self.reservations[t])
+
+    def computeHFromCellToCell(self, i1, j1, i2, j2, options):
+        return abs(i1 - i2) + abs(j1 - j2)
+
+    def startSearch(self, map, options, agent):
+        prevTime = time.time()
+
+        startNode = Node(agent.start_i, agent.start_j, agent.dir)
+        startNode.h = self.computeHFromCellToCell(agent.start_i, agent.start_j, agent.fin_i, agent.fin_j, options)
+        startNode.f = startNode.g + startNode.h
+
+        finNode = Node(agent.fin_i, agent.fin_j, agent.dir)
+
+        self.lppath = []
+        self.hppath = []
+
+        openHeap = []
+        openCopy = dict()
+        closed = set()
+
+        numberOfSteps = 0
+        pathFound = False
+
+        entry = Entry(startNode, None, options)
+        heapq.heappush(openHeap, entry)
+        openCopy[(startNode.i, startNode.j, startNode.dir, startNode.t)] = (startNode.h, startNode.f)
+
+        while (not pathFound) and len(openHeap) > 0:
+            numberOfSteps += 1
+
+            curNode = (heapq.heappop(openHeap)).priority
+
+            if (curNode.i == finNode.i and curNode.j == finNode.j):
+                finNode = curNode
+                pathFound = True
+
+            else:
+                openCopy.pop((curNode.i, curNode.j, curNode.dir, curNode.t))
+                closed.add(curNode)
+
+                successors = self.findSuccessors(curNode, map, options, agent)
+                for i in range(len(successors)):
+                    scNode = successors[i]
+                    foundInClosed = False
+                    if (scNode in closed):
+                        foundInClosed = True
+
+                    if ((not foundInClosed) and curNode.g + 1 <= scNode.g):
+
+                        scNode.g = curNode.g + 1
+                        scNode.h = self.computeHFromCellToCell(scNode.i, scNode.j, agent.fin_i, agent.fin_j, options)
+                        scNode.f = scNode.g + scNode.h
+                        scNode.t = curNode.t + 1
+
+                        scNode.parent = curNode
+
+                        foundInOpen = False
+                        if ((scNode.i, scNode.j, scNode.dir, scNode.t) in openCopy.keys()):
+                            check = openCopy.get((scNode.i, scNode.j, scNode.dir, scNode.t))
+                            checkH = check[0]
+                            checkF = check[1]
+
+                            foundInOpen = True
+
+                        if (foundInOpen == True and checkF > scNode.f):
+                            # удалить старую, вставить новую
+                            checkNode = Node(scNode.i, scNode.j, scNode.dir)
+                            checkNode.t = scNode.t
+                            entry = Entry(checkNode, None, options)
+
+                            openHeap.remove(entry)
+                            heapq.heapify(openHeap)
+
+                            entry = Entry(scNode, None, options)
+                            heapq.heappush(openHeap, entry)
+
+                            openCopy[(scNode.i, scNode.j, scNode.dir, scNode.t)] = (scNode.h, scNode.f)
+
+                        if (foundInOpen == False):
+                            entry = Entry(scNode, None, options)
+                            heapq.heappush(openHeap, entry)
+                            openCopy[(scNode.i, scNode.j, scNode.dir, scNode.t)] = (scNode.h, scNode.f)
+
+        pathLength = 0.0
+
+        if pathFound:
+            pathLength = finNode.g
+            self.makePrimaryPath(finNode, startNode)
+            self.makeFlatlandFriendlyPath(agent)
+
+    def findSuccessors(self, curNode, map, options, agent):
+                global env
+                position = [curNode.i, curNode.j]
+                available = env.rail.get_transitions(*position, curNode.dir)
+                inter_answer = []
+                if (available[0] == True):
+                        inter_answer.append(Node(curNode.i - 1, curNode.j, 0))
+                if (available[1] == True):
+                        inter_answer.append(Node(curNode.i, curNode.j + 1, 1))
+                if (available[2] == True):
+                        inter_answer.append(Node(curNode.i + 1, curNode.j, 2))
+                if (available[3] == True):
+                        inter_answer.append(Node(curNode.i, curNode.j - 1, 3))
+                inter_answer.append(Node(curNode.i, curNode.j, curNode.dir))
+                successors = []
+                for ind in range(len(inter_answer)):
+                        inter_answer[ind].g = curNode.g + 1
+                        inter_answer[ind].h = self.computeHFromCellToCell(inter_answer[ind].i, inter_answer[ind].j, agent.fin_i, agent.fin_j, options)
+                        inter_answer[ind].f = inter_answer[ind].g + inter_answer[ind].h
+                        inter_answer[ind].t = curNode.t + 1
+                        if (not self.checkReservation(inter_answer[ind].i, inter_answer[ind].j, inter_answer[ind].t)): # место не занято
+                                if (self.checkReservation(inter_answer[ind].i, inter_answer[ind].j, inter_answer[ind].t + 1)):
+                                    continue
+                                if (not (self.checkReservation(inter_answer[ind].i, inter_answer[ind].j, curNode.t) and self.checkReservation(curNode.i, curNode.j, inter_answer[ind].t))): #нет edge conflict
+                                        successors.append(inter_answer[ind])
+                return successors
+
+    def makePrimaryPath(self, curNode, startNode):
+        # агент стоит в финишной вершине до конца
+        for i in range(curNode.t, self.maxTime):
+            self.reservations[i].add((curNode.i, curNode.j))
+
+        while curNode != startNode:
+            self.lppath.append(curNode)
+            self.reservations[curNode.t].add((curNode.i, curNode.j))
+            curNode = curNode.parent
+
+        self.lppath.append(curNode)
+        self.reservations[curNode.t].add((curNode.i, curNode.j))
+
+        self.lppath = self.lppath[::-1]
+
+    def makeFlatlandFriendlyPath(self, agent):
+        for ind in range(1, len(self.lppath)):
+            if (self.lppath[ind].i == self.lppath[ind - 1].i and self.lppath[ind].j == self.lppath[ind - 1].j):
+                agent_action[agent.agentId].append(4)
+            elif abs(self.lppath[ind].dir - self.lppath[ind - 1].dir) % 2 == 0:
+                agent_action[agent.agentId].append(2)
+            elif ((self.lppath[ind - 1].dir + 1) % 4 == self.lppath[ind].dir):
+                agent_action[agent.agentId].append(3)
+            else:
+                agent_action[agent.agentId].append(1)
+        
+def build():
+    global agent_action
+    global answer_build
+    global current_pos
+    answer_build = False
+    agent_action = []
+    current_pos = []
+    global env
+    map = Map()
+    map.getMapOptions()
+
+    options = Options()
+    options.getOptions()
+
+    agents = Agents()
+    agents.getAgents()
+
+    for i in range(agents.size):
+        agent = Agent(i)
+        agent.getAgent()
+        agents.allAgents.append(agent)
+        agent_action.append([])
+
+    search = ISearch(options)
+    search.startAllAgents(map, options, agents)
+    
 def my_controller(env, number_of_agents):
     global answer_build
     global agent_action
     global current_pos
     if (answer_build == False):
-        build(env)
+        build()
         current_pos = [0] * env.get_num_agents()
         answer_build = True
     _action = {}
