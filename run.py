18dbd540db5c7d830bd0643026e452bda802d335
vetrov_andrew <vetand2013@yandex.ru>
2019-09-04 16:35:27 +0000

    Update run.py

--- run.py
+++ run.py
@@ -19,45 +19,16 @@ remote_client = FlatlandRemoteClient()
 # compute the necessary action for this step for all (or even some)
 # of the agents
 #####################################################################
-answer_build = False
-agent_action = []
-current_pos = []
 
 class Map:
     def __init__(self):
         self.height = -1
         self.width = -1
-        self.cellSize = -1
-        self.grid = []
 
     def getMapOptions(self):
         global env
-
-        self.height = env.height
-        
+        self.height = env.height  
         self.width = env.width
-
-        self.cellSize = 1
-
-
-class Options:
-    def __init__(self):
-        self.metricType = "manhattan"
-        self.searchType = "astar"
-        self.hweight = 1
-        self.breakingTies = "g-max"
-        self.allowSqueeze = False
-        self.allowDiagonal = False
-        self.cutCorners = False
-
-    def getOptions(self):
-        self.metricType = "manhattan"
-        self.searchType = "astar"
-        self.hweight = 1
-        self.breakingTies = "g-max"
-        self.allowSqueeze = False
-        self.allowDiagonal = False
-        self.cutCorners = False
 
 
 class Agent:
@@ -119,18 +90,15 @@ class Node:
 
 
 class Entry:
-    def __init__(self, priority, value, options):
+    def __init__(self, priority, value):
         self.priority = priority
         self.value = value
-        self.breakingTies = options.breakingTies
 
     def cmp(self, one, two):
         if (one.f == two.f):
             if (one.g == two.g):
                 return one < two
             else:
-                if (self.breakingTies == "g-min"):
-                    return one.g < two.g
                 return one.g > two.g
         return one.f < two.f
 
@@ -145,33 +113,19 @@ class Entry:
 
 
 class ISearch:
-    def __init__(self, options):
+    def __init__(self):
         global env
         self.lppath = []
         self.hppath = []
-        self.printGrid = []
         self.reservations = [set()]
-        self.breakingTies = options.breakingTies
-        self.sqrtTwo = 1.41421356237
         self.maxTime = 400
 
-    def startAllAgents(self, map, options, agents):
+    def startAllAgents(self, map, agents):
         self.initReservations(map)
 
         for i in range(agents.size):
-            self.startSearch(map, options, agents.allAgents[i])
+            self.startSearch(map, agents.allAgents[i])
 
-    def initPrintGrid(self, map):
-        self.printGrid.clear()
-        for i in range(map.height):
-            line = [0] * map.width
-            for j in range(map.width):
-                val = map.getValue(i, j)
-                if val == 0:
-                    line[j] = -2
-                else:
-                    line[j] = -1
-            self.printGrid.append(line)
 
     def initReservations(self, map):
         self.reservations = sets = [set() for i in range(self.maxTime)]
@@ -179,14 +133,13 @@ class ISearch:
     def checkReservation(self, i, j, t):
         return ((i, j) in self.reservations[t])
 
-    def computeHFromCellToCell(self, i1, j1, i2, j2, options):
+    def computeHFromCellToCell(self, i1, j1, i2, j2):
         return abs(i1 - i2) + abs(j1 - j2)
 
-    def startSearch(self, map, options, agent):
-        prevTime = time.time()
+    def startSearch(self, map, agent):
 
         startNode = Node(agent.start_i, agent.start_j, agent.dir)
-        startNode.h = self.computeHFromCellToCell(agent.start_i, agent.start_j, agent.fin_i, agent.fin_j, options)
+        startNode.h = self.computeHFromCellToCell(agent.start_i, agent.start_j, agent.fin_i, agent.fin_j)
         startNode.f = startNode.g + startNode.h
 
         finNode = Node(agent.fin_i, agent.fin_j, agent.dir)
@@ -198,15 +151,13 @@ class ISearch:
         openCopy = dict()
         closed = set()
 
-        numberOfSteps = 0
         pathFound = False
 
-        entry = Entry(startNode, None, options)
+        entry = Entry(startNode, None)
         heapq.heappush(openHeap, entry)
         openCopy[(startNode.i, startNode.j, startNode.dir, startNode.t)] = (startNode.h, startNode.f)
 
         while (not pathFound) and len(openHeap) > 0:
-            numberOfSteps += 1
 
             curNode = (heapq.heappop(openHeap)).priority
 
@@ -218,7 +169,7 @@ class ISearch:
                 openCopy.pop((curNode.i, curNode.j, curNode.dir, curNode.t))
                 closed.add(curNode)
 
-                successors = self.findSuccessors(curNode, map, options, agent)
+                successors = self.findSuccessors(curNode, map, agent)
                 for i in range(len(successors)):
                     scNode = successors[i]
                     foundInClosed = False
@@ -228,7 +179,7 @@ class ISearch:
                     if ((not foundInClosed) and curNode.g + 1 <= scNode.g):
 
                         scNode.g = curNode.g + 1
-                        scNode.h = self.computeHFromCellToCell(scNode.i, scNode.j, agent.fin_i, agent.fin_j, options)
+                        scNode.h = self.computeHFromCellToCell(scNode.i, scNode.j, agent.fin_i, agent.fin_j)
                         scNode.f = scNode.g + scNode.h
                         scNode.t = curNode.t + 1
 
@@ -243,32 +194,32 @@ class ISearch:
                             foundInOpen = True
 
                         if (foundInOpen == True and checkF > scNode.f):
-                            # удалить старую, вставить новую
                             checkNode = Node(scNode.i, scNode.j, scNode.dir)
                             checkNode.t = scNode.t
-                            entry = Entry(checkNode, None, options)
+                            entry = Entry(checkNode, None)
 
                             openHeap.remove(entry)
                             heapq.heapify(openHeap)
 
-                            entry = Entry(scNode, None, options)
+                            entry = Entry(scNode, None)
                             heapq.heappush(openHeap, entry)
 
                             openCopy[(scNode.i, scNode.j, scNode.dir, scNode.t)] = (scNode.h, scNode.f)
 
                         if (foundInOpen == False):
-                            entry = Entry(scNode, None, options)
+                            entry = Entry(scNode, None)
                             heapq.heappush(openHeap, entry)
                             openCopy[(scNode.i, scNode.j, scNode.dir, scNode.t)] = (scNode.h, scNode.f)
-
-        pathLength = 0.0
+        
 
         if pathFound:
             pathLength = finNode.g
             self.makePrimaryPath(finNode, startNode)
             self.makeFlatlandFriendlyPath(agent)
+        else:
+            print(1 / 0) # trigger for unfound path
 
-    def findSuccessors(self, curNode, map, options, agent):
+    def findSuccessors(self, curNode, map, agent):
                 global env
                 position = [curNode.i, curNode.j]
                 available = env.rail.get_transitions(*position, curNode.dir)
@@ -283,20 +234,19 @@ class ISearch:
                         inter_answer.append(Node(curNode.i, curNode.j - 1, 3))
                 inter_answer.append(Node(curNode.i, curNode.j, curNode.dir))
                 successors = []
-                for ind in range(len(inter_answer)):
-                        inter_answer[ind].g = curNode.g + 1
-                        inter_answer[ind].h = self.computeHFromCellToCell(inter_answer[ind].i, inter_answer[ind].j, agent.fin_i, agent.fin_j, options)
-                        inter_answer[ind].f = inter_answer[ind].g + inter_answer[ind].h
-                        inter_answer[ind].t = curNode.t + 1
-                        if (not self.checkReservation(inter_answer[ind].i, inter_answer[ind].j, inter_answer[ind].t)): # место не занято
-                                if (self.checkReservation(inter_answer[ind].i, inter_answer[ind].j, inter_answer[ind].t + 1)):
+                for scNode in inter_answer:
+                        scNode.g = curNode.g + 1
+                        scNode.h = self.computeHFromCellToCell(scNode.i, scNode.j, agent.fin_i, agent.fin_j)
+                        scNode.f = scNode.g + scNode.h
+                        scNode.t = curNode.t + 1
+                        if (not self.checkReservation(scNode.i, scNode.j, scNode.t)):
+                                if (self.checkReservation(scNode.i, scNode.j, scNode.t + 1)):
                                     continue
-                                if (not (self.checkReservation(inter_answer[ind].i, inter_answer[ind].j, curNode.t) and self.checkReservation(curNode.i, curNode.j, inter_answer[ind].t))): #нет edge conflict
-                                        successors.append(inter_answer[ind])
+                                if (not (self.checkReservation(scNode.i, scNode.j, curNode.t) and self.checkReservation(curNode.i, curNode.j, scNode.t))):
+                                        successors.append(scNode)
                 return successors
 
     def makePrimaryPath(self, curNode, startNode):
-        # агент стоит в финишной вершине до конца
         for i in range(curNode.t, self.maxTime):
             self.reservations[i].add((curNode.i, curNode.j))
 
@@ -313,66 +263,55 @@ class ISearch:
     def makeFlatlandFriendlyPath(self, agent):
         for ind in range(1, len(self.lppath)):
             if (self.lppath[ind].i == self.lppath[ind - 1].i and self.lppath[ind].j == self.lppath[ind - 1].j):
-                agent_action[agent.agentId].append(4)
+                path_finder.agent_action[agent.agentId].append(4)
             elif abs(self.lppath[ind].dir - self.lppath[ind - 1].dir) % 2 == 0:
-                agent_action[agent.agentId].append(2)
+                path_finder.agent_action[agent.agentId].append(2)
             elif ((self.lppath[ind - 1].dir + 1) % 4 == self.lppath[ind].dir):
-                agent_action[agent.agentId].append(3)
+                path_finder.agent_action[agent.agentId].append(3)
             else:
-                agent_action[agent.agentId].append(1)
+                path_finder.agent_action[agent.agentId].append(1)
+
+class solver:
+    def __init__(self):
+        self.answer_build = False
+        self.agent_action = []
+        self.current_pos = []
+        self.map = Map()
+        self.agents = Agents()
+        self.search = ISearch()
+
+    def build(self):
+        global env
+        self.answer_build = True
+        self.current_pos = [0] * env.get_num_agents()
+        self.agent_action = []
+        self.map.getMapOptions()
+
+        self.agents.getAgents()
+
+        for i in range(self.agents.size):
+            agent = Agent(i)
+            agent.getAgent()
+            self.agents.allAgents.append(agent)
+            self.agent_action.append([])
+
+        self.search.startAllAgents(self.map, self.agents)
         
-def build():
-    global agent_action
-    global answer_build
-    global current_pos
-    answer_build = False
-    agent_action = []
-    current_pos = []
-    global env
-    map = Map()
-    map.getMapOptions()
-
-    options = Options()
-    options.getOptions()
-
-    agents = Agents()
-    agents.getAgents()
-
-    for i in range(agents.size):
-        agent = Agent(i)
-        agent.getAgent()
-        agents.allAgents.append(agent)
-        agent_action.append([])
-
-    search = ISearch(options)
-    search.startAllAgents(map, options, agents)
+    def print_step(self):
+        _action = {}
+        for ind in range(env.get_num_agents()):
+            if (self.current_pos[ind] < len(self.agent_action[ind])):
+                _action[ind] = self.agent_action[ind][self.current_pos[ind]]
+                self.current_pos[ind] += 1
+        return _action
     
 def my_controller(env, number_of_agents):
-    global answer_build
-    global agent_action
-    global current_pos
-    if (answer_build == False):
-        build()
-        current_pos = [0] * env.get_num_agents()
-        answer_build = True
-    _action = {}
-    for ind in range(env.get_num_agents()):
-        if (current_pos[ind] < len(agent_action[ind])):
-            _action[ind] = agent_action[ind][current_pos[ind]]
-            current_pos[ind] += 1
+    if (path_finder.answer_build == False):
+        path_finder.build()
+    _action = path_finder.print_step()
     return _action
 
-#####################################################################
-# Instantiate your custom Observation Builder
-# 
-# You can build your own Observation Builder by following 
-# the example here : 
-# https://gitlab.aicrowd.com/flatland/flatland/blob/master/flatland/envs/observations.py#L14
-#####################################################################
-my_observation_builder = TreeObsForRailEnv(
-                                max_depth=3,
-                                predictor=ShortestPathPredictorForRailEnv()
-                            )
+path_finder = solver()
 
 #####################################################################
 # Main evaluation loop
@@ -382,10 +321,8 @@ my_observation_builder = TreeObsForRailEnv(
 evaluation_number = 0
 while True:
 
-    answer_build = False
-    agent_action = []
-    current_pos = []
     evaluation_number += 1
+    path_finder.answer_build = False
     # Switch to a new evaluation environemnt
     # 
     # a remote_client.env_create is similar to instantiating a 
