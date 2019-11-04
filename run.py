64c75fa4f0a9ae537b21cea85e95c3b6f9e169fa
Vetand <vetand2013@yandex.ru>
2019-11-04 20:57:35 +0000

    Update run.py

--- run.py
+++ run.py
@@ -563,8 +563,11 @@ class submission:
         best_solution = INFINITY
         best_actions = self.control_agent
         self.set_obligations()
-        for attempt in range(1): # we choose agents which had the longest delays and move them to the top of the queue (within one speed value)
-            path_exists = self.build_with_order(self.current_order, 250, (attempt == 0))
+        for attempt in range(2): # we choose agents which had the longest delays and move them to the top of the queue (within one speed value)
+                                 # there are two attempts which we calculate, then chose the best
+                                 # on the turn number 4 we add more agents to the best solution
+                                 # on the half of the step limit we add malfunctioning agents and hope that they won`t break path calculated here
+            path_exists = self.build_with_order(self.current_order, 125, (attempt == 0))
             if (self.overall_reward() < best_solution):
                 best_solution = self.overall_reward()
                 best_actions = copy.deepcopy(self.control_agent)
@@ -597,7 +600,7 @@ class submission:
         self.set_obligations()
         self.control_agent.getAgents(self.env)
         for attempt in range(10):
-            if (((self.current_step - 10) * 8) // 5 < self.maxStep): # malfunctioning agents enter the environment, we can afford not all of them, the maximumm planning time is 15 seconds
+            if ((self.current_step - 10) * 2 < self.maxStep): # malfunctioning agents enter the environment, we can afford not all of them, the maximumm planning time is 15 seconds
                 path_exists = self.build_with_order_malfunctioning(self.current_order_malfunctions, 8)
                 new_order = []
                 for ind in range(len(self.current_order_malfunctions)):
@@ -665,14 +668,14 @@ class submission:
                 new_order.append(number)
         self.current_order = copy.deepcopy(new_order)
         self.set_obligations()
-        self.build_with_order(self.current_order, 150, False)
+        self.build_with_order(self.current_order, 250, False)
 
 def my_controller(env, path_finder):
     if (path_finder.answer_build == False):
         path_finder.build()
-    if (path_finder.current_step == path_finder.maxStep // 10): # additional placement of non-malfunctioning agents (as the result of small time limits on the first step)
+    if (path_finder.current_step == 4): # additional placement of non-malfunctioning agents (as the result of small time limits on the first step)
         path_finder.reset_third()
-    if (path_finder.overall_time <= 800 and path_finder.current_step >= (path_finder.maxStep * 5) // 8 and path_finder.current_step % 10 == 0): # re-plan paths every 10 steps
+    if (path_finder.overall_time <= 900 and path_finder.current_step >= path_finder.maxStep // 2 and path_finder.current_step % 10 == 0): # re-plan paths every 10 steps
         path_finder.build_malfunctioning()
     return path_finder.print_step()
 
